5e89199ae3e2df2910e7b0a58f1855b1fede903c
shovickbarua <shovickbarua92@gmail.com>
2024-09-16 20:29:56 +0600

    update

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,19 @@
+<div>
+    <nav class="bg-white shadow-md">
+        <div class="container mx-auto px-4 py-2 flex justify-end">
+            <button class="text-gray-800 hover:text-blue-500 px-4 py-2">Login</button>
+            <button class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md ml-2">Get Started</button>
+        </div>
+    </nav>
+    <div class="bg-url bg-cover bg-center h-screen w-full">
+        <div class='flex px-4 py-5 justify-end'>
+            <button class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md ml-2">Export PDF</button>
+            <button class="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded-md ml-2">Export EXCEL</button>
+        </div>
+        <div>
+            
+        </div>
+    </div>
+
+    {{-- Success is as dangerous as failure. --}}
+</div>
